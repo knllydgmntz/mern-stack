9920298d17e96c34a01a46481d3cb6864abc1a39
knllydgmntz <knllydgmntz@gmail.com>
2024-05-13 14:15:22 +0800

    set up express async in backend

---
+++ backend/controllers/noteController.js
@@ -0,0 +1,40 @@
+const asyncHandler = require("express-async-handler");
+
+// @desc Get notes
+// @route GET /api/notes
+// @access Private
+const getNotes = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Get notes" });
+});
+
+// @desc Set notes
+// @route POST /api/notes
+// @access Private
+const setNote = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
+  res.status(200).json({ message: "Set note" });
+});
+
+// @desc Update note
+// @route PUT /api/notes/:id
+// @access Private
+const updateNote = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Update note ${req.params.id}` });
+});
+
+// @desc delete note
+// @route DELETE /api/notes/:id
+// @access Private
+const deleteNote = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Delete note ${req.params.id}` });
+});
+
+module.exports = {
+  getNotes,
+  setNote,
+  updateNote,
+  deleteNote,
+};
